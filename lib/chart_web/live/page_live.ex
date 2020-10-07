413335ecbb051456e2c4f119e0eaa6503b5be593
Boobalan AP <apboobalan@gmail.com>
2020-10-07 22:33:17 +0530

    Finish push event.

--- lib/chart_web/live/page_live.ex
+++ lib/chart_web/live/page_live.ex
@@ -3,6 +3,16 @@ defmodule ChartWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    schedule_update()
     {:ok, socket}
   end
+
+  @impl true
+  def handle_info(:update, socket) do
+    schedule_update()
+    {:noreply, socket |> push_event("points", %{points: get_points()})}
+  end
+
+  defp schedule_update, do: self() |> Process.send_after(:update, 2000)
+  defp get_points, do: 1..7 |> Enum.map(fn _ -> :rand.uniform(100) end)
 end
